1edf495c877dc677aa8888a8c6f1435d73135888
Bit_Blitz <rachit.tiwari.07.off@gmail.com>
2026-05-11 11:34:31 +0530

    chore: enforce text/plain for apple pay verification

--- next.config.mjs
+++ next.config.mjs
@@ -6,6 +6,19 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
+  async headers() {
+    return [
+      {
+        source: '/.well-known/apple-developer-merchantid-domain-association',
+        headers: [
+          {
+            key: 'Content-Type',
+            value: 'text/plain',
+          },
+        ],
+      },
+    ]
+  },
 }
 
 export default nextConfig
