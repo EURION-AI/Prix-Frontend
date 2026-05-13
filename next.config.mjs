7e30a71a8a13c26e3db36a028a864e779f24c037
EURION-AI <socials@prixai.xyz>
2026-05-13 23:44:15 +0530

    Add files via upload

--- next.config.mjs
+++ next.config.mjs
@@ -9,15 +9,6 @@ const nextConfig = {
   async headers() {
     return [
       {
-        source: '/sitemap.xml',
-        headers: [
-          {
-            key: 'Cache-Control',
-            value: 'public, max-age=0, s-maxage=3600, must-revalidate',
-          },
-        ],
-      },
-      {
         source: '/.well-known/apple-developer-merchantid-domain-association',
         headers: [
           {
