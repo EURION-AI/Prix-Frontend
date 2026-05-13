6a50c9f45809aeb3fadbaf31f3c3c9c23d117837
EURION-AI <socials@prixai.xyz>
2026-05-13 22:31:53 +0530

    Add files via upload

--- middleware.ts
+++ middleware.ts
@@ -58,6 +58,6 @@ export function middleware(request: NextRequest) {
 
 export const config = {
   matcher: [
-    '/((?!_next/static|_next/image|favicon.ico|icon.*|apple-icon.*|.*\\.png$|\\.well-known/.*).*)',
+    '/((?!_next/static|_next/image|favicon.ico|icon.*|apple-icon.*|sitemap.xml|robots.txt|.*\\.png$|\\.well-known/.*).*)',
   ],
 }
